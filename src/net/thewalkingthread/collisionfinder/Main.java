b57bc32e26f2a447b7a22ad0b49eefcdbd8469df
Zoltán Sasvári <z.sasvari@student.tugraz.at>
2015-12-29 17:08:55 +0100

    now using bufferedreader instead of scanner for better performance

--- src/net/thewalkingthread/collisionfinder/Main.java
+++ src/net/thewalkingthread/collisionfinder/Main.java
@@ -1,23 +1,47 @@
 package net.thewalkingthread.collisionfinder;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) throws Exception{
-        SHAWorker.init("13304351232056");
-        int hash_counter = 0;
-        String hash, input;
-        FileHandler.init("/home/zoli/Desktop/test.txt");
-        //noinspection InfiniteLoopStatement
-        while (true){
-            SHAWorker.computeHash();
-            hash = SHAWorker.getHash();
-            input = SHAWorker.getInput();
-            FileHandler.addHash(hash, input);
-            hash_counter++;
-            if (hash_counter == 5000000){
-                hash_counter = 0;
-                FileHandler.writeHash();
+    public static void main(String[] args) throws Exception {
+        if (args[0].equals("-g")){
+            SHAWorker.init("13304351232056");
+            int hash_counter = 0;
+            String hash, input;
+            FileHandler.init("/Volumes/RuggedHDD/Dokumente/hash.txt");
+            //noinspection InfiniteLoopStatement
+            while (true){
+                SHAWorker.computeHash();
+                hash = SHAWorker.getHash();
+                input = SHAWorker.getInput();
+                FileHandler.addHash(hash, input);
+                hash_counter++;
+                if (hash_counter == 5000000){
+                    hash_counter = 0;
+                    FileHandler.writeHash();
+                }
             }
+        }
+        if (args[0].equals("-c")){
+            BufferedReader scanner = new BufferedReader(new FileReader("/Volumes/RuggedHDD/Dokumente/hash_sorted.txt"));
+            String prev = scanner.readLine(), line = scanner.readLine();
+            String prev_hash = prev.split("-")[0], line_hash = line.split("-")[0];
+            while (line != null){
+                if (prev_hash.equals(line_hash)){
+                    System.out.println("Match found");
+                    System.out.println("Hash 1: "+prev);
+                    System.out.println("Hash 2: "+line);
+                }
+                prev = line;
+                prev_hash = line_hash;
+                line = scanner.readLine();
+                line_hash = line.split("-")[0];
+            }
+            System.out.println("No match");
         }
     }
 }

f9dfbaad656bc48ec1cdae30f89cec8b8a6548d7
sasvarizoli <sasvarizolime.com>
2016-01-06 23:20:42 +0100

    optimized hash search

--- src/net/thewalkingthread/collisionfinder/Main.java
+++ src/net/thewalkingthread/collisionfinder/Main.java
@@ -12,7 +12,7 @@ public class Main {
             SHAWorker.init("13304351232056");
             int hash_counter = 0;
             String hash, input;
-            FileHandler.init("/Volumes/RuggedHDD/Dokumente/hash.txt");
+            FileHandler.init("/media/zoli/RuggedHDD/Dokumente/hash.txt");
             //noinspection InfiniteLoopStatement
             while (true){
                 SHAWorker.computeHash();
@@ -20,28 +20,26 @@ public class Main {
                 input = SHAWorker.getInput();
                 FileHandler.addHash(hash, input);
                 hash_counter++;
-                if (hash_counter == 10000000){
+                if (hash_counter == 5000000){
                     hash_counter = 0;
                     FileHandler.writeHash();
                 }
             }
         }
         if (args[0].equals("-c")){
-            BufferedReader scanner = new BufferedReader(new FileReader("/Volumes/RuggedHDD/Dokumente/hash_sorted.txt"));
-            String prev = scanner.readLine(), line = scanner.readLine();
-            String prev_hash = prev.split("-")[0], line_hash = line.split("-")[0];
-            System.out.println(prev_hash);
+            BufferedReader scanner = new BufferedReader(new FileReader("/media/zoli/RuggedHDD/Dokumente/hash_sorted.txt"));
+            String[] prev = scanner.readLine().split("-"), line = scanner.readLine().split("-");
             while (true){
-                if (prev_hash.equals(line_hash)){
+                if (prev[0].equals(line[0]) && !prev[1].equals(line[1])){
                     System.out.println("Match found");
-                    System.out.println("Hash 1: "+prev);
-                    System.out.println("Hash 2: "+line);
+                    System.out.println("Hash: "+prev[0]);
+                    System.out.println("In 1: "+prev[1]);
+                    System.out.println("In 2: "+line[1]);
                 }
                 prev = line;
-                prev_hash = line_hash;
-                line = scanner.readLine();
-                if (line == null) break;
-                line_hash = line.split("-")[0];
+                String line_raw = scanner.readLine();
+                if (line_raw == null) break;
+                line = line_raw.split("-");
             }
             System.out.println("No match");
         }
